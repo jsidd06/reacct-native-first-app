56b22ecfe32aa6e44736cca7b5dd29e13961fe04
siddharth <jsidd06@gmail.com>
2021-12-29 10:59:16 +0530

    start react native

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
+  const [order,setOrder] = useState('this is favorite');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{order}</Text>
+      <Button title="change" onPress={()=>setOrder('no')}/>
       <StatusBar style="auto" />
     </View>
   );
