48c4f05a4f14382f048a9ca00050efb9d4da7c36
siddharth <jsidd06@gmail.com>
2021-12-30 14:00:22 +0530

    set input field

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, View,Button,TextInput } from 'react-native';
 
 export default function App() {
-  const [order,setOrder] = useState('this is favorite');
   return (
-    <View style={styles.container}>
-      <Text>{order}</Text>
-      <Button title="change" onPress={()=>setOrder('no')}/>
-      <StatusBar style="auto" />
+    <View style={{padding:50}} >
+      <View style={{flexDirection:"row",justifyContent:"space-between",alignItems:"center"}}>
+      <TextInput placeholder="Enter your name" style={{width:"80%",borderColor:"black", borderWidth:1,textAlign:"center"}} />
+      <Button title="ADD" />
+      </View>
+      <View />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
